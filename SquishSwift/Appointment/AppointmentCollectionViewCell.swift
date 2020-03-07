580d9b836249b3478ad2d865f740f603618f6493
MaxFritzhand <mfritzhand1@gmail.com>
2020-03-07 15:06:54 -0500

    SquishSupport is a Mental Health Platform connecting everyday people together based on common interests. (iOS)

---
+++ SquishSwift/Appointment/AppointmentCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  AppointmentCollectionViewCell.swift
+//  SquishSwift
+//
+//  Created by Andrew Hong on 12/17/19.
+//  Copyright © 2019 Andrew Hong. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ SquishSwift/Calendar/DateHeader.swift
@@ -0,0 +1,17 @@
+//
+//  DateHeader.swift
+//  SquishSwift
+//
+//  Created by Andrew Hong on 11/25/19.
+//  Copyright © 2019 Andrew Hong. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class DateHeader: JTACMonthReusableView {
+    @IBOutlet var monthTitle: UILabel!
+    @IBOutlet var yearTitle: UILabel!
+    
+    
+}
